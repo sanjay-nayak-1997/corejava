2b5371d0dd8037eecba7390b58c4723d0db2cbf0
sanjay-nayak-1997 <41293647+sanjay-nayak-1997@users.noreply.github.com>
2018-07-18 23:10:21 +0530

    Add files via upload

---
+++ scenarioFruit.java
@@ -0,0 +1,21 @@
+
+public class scenarioFruit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("The fruit name is :-");
+		char a=(char)77;
+		char b=(char)65;
+		char c=(char)78;
+		char d=(char)71;
+		char e=(char)79;
+		
+		System.out.print(a);
+		System.out.print(b);
+		System.out.print(c);
+		System.out.print(d);
+		System.out.print(e);
+
+	}
+
+}
