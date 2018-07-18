1c042949034bbf30b5154a877a861cc7c9f0f596
sanjay-nayak-1997 <41293647+sanjay-nayak-1997@users.noreply.github.com>
2018-07-18 21:29:35 +0530

    Add files via upload

---
+++ scenarioSpeed.java
@@ -0,0 +1,15 @@
+package github;
+
+public class scenarioSpeed {
+
+	public static void main(String[] args) {
+
+		long distance=689232;
+		
+		long noOfHrs=5;
+		
+		System.out.println("The distance travelled is " + (distance*noOfHrs));
+
+	}
+
+}
