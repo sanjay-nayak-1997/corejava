f4e7f527661879e8258b81328e14fbdea3227e73
sanjay-nayak-1997 <41293647+sanjay-nayak-1997@users.noreply.github.com>
2018-07-18 23:36:08 +0530

    Add files via upload

---
+++ scenarioAnimal.java
@@ -0,0 +1,18 @@
+
+public class scenarioAnimal {
+
+	public static void main(String[] args) {
+		
+		int a=(int)'L';
+		int b=(int)'I';
+		int c=(int)'O';
+		int d=(int)'N';
+		
+		System.out.println("The ASCII numbers for each characters in the word \'LION\' is :-");
+		System.out.println(a + " " + b + "" + " " + c + " " + d);
+		
+		
+
+	}
+
+}
