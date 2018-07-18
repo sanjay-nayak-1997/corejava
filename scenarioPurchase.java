42eb6c488569c83aba027e81bd6cf7ac8f6477ff
sanjay-nayak-1997 <41293647+sanjay-nayak-1997@users.noreply.github.com>
2018-07-18 21:53:17 +0530

    Add files via upload

---
+++ scenarioPurchase.java
@@ -0,0 +1,16 @@
+package github;
+
+public class scenarioPurchase {
+
+	public static void main(String[] args) {
+		
+		int amount=1000;
+		
+		int cost=75;
+		
+		System.out.println("The number of ties that Sneha can buy is " + amount/cost);
+		System.out.println("The amount left with her is " + amount%cost);
+
+	}
+
+}
