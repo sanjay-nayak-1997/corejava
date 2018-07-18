3c165b78781bde935ad8e7b2df6d77a771a2835d
sanjay-nayak-1997 <41293647+sanjay-nayak-1997@users.noreply.github.com>
2018-07-18 21:49:42 +0530

    Add files via upload

---
+++ scenarioPhones.java
@@ -0,0 +1,14 @@
+package github;
+
+public class scenarioPhones {
+
+	public static void main(String[] args) {
+		
+		double amount=5000.0, cost=650.0;
+		
+		System.out.println("The amount left with him after the purchase is " + (amount%cost));
+		
+
+	}
+
+}
