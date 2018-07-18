b356045258e1029e3a76c728de402a5af2d3e776
sanjay-nayak-1997 <41293647+sanjay-nayak-1997@users.noreply.github.com>
2018-07-18 21:45:45 +0530

    Add files via upload

---
+++ scenarioGrade.java
@@ -0,0 +1,16 @@
+package github;
+
+public class scenarioGrade {
+
+	public static void main(String[] args) {
+		
+		
+		double English=95,Math=45,Science=88;
+		
+		Math=Math*(100/50);
+		
+		System.out.println("The percentage of marks that he scored is " + (English+Math+Science)/3);
+		
+	}
+
+}
