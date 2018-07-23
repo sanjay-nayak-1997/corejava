466caadf2ccad09c83ec0f4118918327028b96f6
sanjay-nayak-1997 <41293647+sanjay-nayak-1997@users.noreply.github.com>
2018-07-23 11:51:04 +0530

    Add files via upload

---
+++ testInterface.java
@@ -0,0 +1,20 @@
+package github;
+
+public class testInterface{
+	
+	public static void main(String[] args) {
+		
+		classInterface i=new classInterface();
+		
+		i.switchon();
+		System.out.println();
+		i.changeChannel();
+		System.out.println();
+		i.increaseVolume();
+		System.out.println();
+		i.decreaseVolume();
+		System.out.println();
+		i.switchOff();
+	}
+	
+}
